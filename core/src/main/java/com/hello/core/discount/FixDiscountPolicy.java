69940d5eee2bc2567fd8e75bed8164672090c77f
jerome <alkwen0996@naver.com>
2024-06-28 17:26:39 +0900

    feat: 할인, 주문 도메인 작성 및 테스트

--- core/src/main/java/com/hello/core/discount/FixDiscountPolicy.java
+++ core/src/main/java/com/hello/core/discount/FixDiscountPolicy.java
@@ -3,14 +3,14 @@ package com.hello.core.discount;
 import com.hello.core.member.Grade;
 import com.hello.core.member.Member;
 
-public class FixDiscountPolicy implements DiscountPolicy{
+public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000원 할인
 
     @Override
     public int discount(final Member member, final int price) {
-        if(member.getGrade() == Grade.VIP){
-
+        if (member.getGrade() == Grade.VIP) { // enum은 == 으로 비교
+            return discountFixAmount;
         }
 
         return 0;
