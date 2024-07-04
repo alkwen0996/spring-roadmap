ea8bea2c8e9fafa4ac52a448e2855c25575d71b4
jerome <alkwen0996@naver.com>
2024-07-04 10:58:04 +0900

    feat: 새로운 할인 정책 개발

--- core/src/main/java/com/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/com/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package com.hello.core.order;
 
 import com.hello.core.discount.DiscountPolicy;
-import com.hello.core.discount.FixDiscountPolicy;
 import com.hello.core.member.Member;
 import com.hello.core.member.MemberRepository;
 import com.hello.core.member.MemoryMemberRepository;
@@ -9,8 +8,9 @@ import com.hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); -> 구체 클래스에 의존 DIP 위반.
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(final Long memberId, final String itemName, final int itemPrice) {
