58eb8536ec9e4878ee0eaa9c1642791045c3f687
jerome <alkwen0996@naver.com>
2024-07-04 21:36:05 +0900

    refactor: 스프링 컨테이너 적용

--- core/src/main/java/com/hello/core/AppConfig.java
+++ core/src/main/java/com/hello/core/AppConfig.java
@@ -7,20 +7,27 @@ import com.hello.core.member.MemberServiceImpl;
 import com.hello.core.member.MemoryMemberRepository;
 import com.hello.core.order.OrderService;
 import com.hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig { // 객체의 생성과 연결 담당 -> DIP 완성
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy()); // 생성자 주입
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
