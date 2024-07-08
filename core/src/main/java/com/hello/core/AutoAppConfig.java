a1ab74512be7b7deea6d04b28cca80c3ab76cfe0
jerome <alkwen0996@naver.com>
2024-07-08 12:01:40 +0900

    의존관계 자동 주입 - 조회한 빈 모두 필요할 때 List, Map

--- core/src/main/java/com/hello/core/AutoAppConfig.java
+++ core/src/main/java/com/hello/core/AutoAppConfig.java
@@ -1,6 +1,14 @@
 package com.hello.core;
 
 
+import com.hello.core.discount.DiscountPolicy;
+import com.hello.core.discount.RateDiscountPolicy;
+import com.hello.core.member.MemberService;
+import com.hello.core.member.MemberServiceImpl;
+import com.hello.core.member.MemoryMemberRepository;
+import com.hello.core.order.OrderService;
+import com.hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,11 +17,10 @@ import static org.springframework.context.annotation.ComponentScan.*;
 
 @Configuration
 @ComponentScan
-//        (basePackages = "com.hello.core.member", // 탐색을 시작할 위치를 지정한다. -> 탐색 범위 최소화
-//        basePackageClasses = AutoAppConfig.class, // 지정한 클래스의 패키지를 탐색 시작 위치로 지정한다
-//        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+        (basePackages = "com.hello.core.member", // 탐색을 시작할 위치를 지정한다. -> 탐색 범위 최소화
+        basePackageClasses = AutoAppConfig.class, // 지정한 클래스의 패키지를 탐색 시작 위치로 지정한다
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 // 기존 AppConfig 빈 등록 방지를 위해(테스트 목적)
 // 최근에는 설정 정보 클래스의 위치를 프로젝트 최상단에 위치시키는 방식으로 한다. 스프링 부트도 이 방법을 기본으로 제공한다.
 public class AutoAppConfig {
-
 }
